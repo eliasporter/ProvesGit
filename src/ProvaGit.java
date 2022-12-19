20861d5a5341096b4da49fdd9edc5cba556066a8
eporter82 <eliasporter@gmail.com>
2022-12-19 16:25:06 +0100

    Mostrem n augmentant 10 vegades

--- src/ProvaGit.java
+++ src/ProvaGit.java
@@ -3,6 +3,12 @@ public class ProvaGit {
 
 	public static void main(String[] args) {
 		System.out.println("Proves i mes proves");
+		
+		int n=0;
+		for (int i = 0; i < 10; i++) {
+			System.out.println(n);
+			n++;
+		}
 
 	}
 
